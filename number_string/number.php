e4beec050691f091ea84052234bc9f52e657d3e5
KDY <merry4739@naver.com>
2022-04-13 23:18:53 +0900

    php 생활 코딩 공부 시작

---
+++ number_string/number.php
@@ -0,0 +1,5 @@
+<?php
+echo 8-4;
+var_dump(6);
+var_dump(6.1);
+?>
---
+++ number_string/string.php
@@ -0,0 +1,14 @@
+<?php
+//다 가능
+echo "hello world";
+echo '"hello world"';
+echo 'hello world';
+echo "'hello world'";
+echo "\"hello world\"";
+echo '\'hello world\'';
+
+echo var_dump(1234);
+echo var_dump("1234");
+echo "hello" . " " . "world";
+echo '그는 "안녕하세요 라고 말했다"';
+?>
