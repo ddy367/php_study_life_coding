fd0c5a7277b7f3f65cd0c3e96f9fe1144fc8ce18
KDY <merry4739@naver.com>
2022-04-14 00:52:06 +0900

    04.14/16

---
+++ IO_form/25.php
@@ -0,0 +1,4 @@
+<?php
+#http://localhost/IO_form/25.php?id=aaa
+echo "welcom, ".$_GET['name']."님 안녕하세요";   #aaa
+?>
---
+++ compare/compare5.php
@@ -0,0 +1,6 @@
+<?php
+echo "1 == '1' : ";
+var_dump(1 == '1');         #true
+echo "<br />1 === '1' : ";
+var_dump(1 === '1');        #false
+?>
